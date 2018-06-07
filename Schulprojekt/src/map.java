efb7d7bfb41158fa834207627a1aafb94b600e65
LucaKleck <LucaKleck@web.de>
2018-06-07 16:32:20 +0200

    tempMap

--- Schulprojekt/src/map.java
+++ Schulprojekt/src/map.java
@@ -2,18 +2,18 @@
 public class map {
 	private MapTile[][] map;
 	
-	public map(MapTile[][] finishedMap) {
-		copyMap(finishedMap);
+	public map(MapTile[][] tempMap) {
+		copyMap(tempMap);
 	}
 	public MapTile[][] getMap() {
 		return map;
 	}
-	private void copyMap(MapTile[][] finishedMap) {
-		map = new MapTile[finishedMap.length][finishedMap[0].length];
+	private void copyMap(MapTile[][] tempMap) {
+		map = new MapTile[tempMap.length][tempMap[0].length];
 		
 		for(int y=0; y < map[0].length; y++) {
 			for(int x=0; x < map.length; x++) {
-				map[x][y] = finishedMap[x][y];
+				map[x][y] = tempMap[x][y];
 			}
 		}
 	}
