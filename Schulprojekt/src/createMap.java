2548f2cbd54cf39b70745f96592c29fa2849347d
LucaKleck <LucaKleck@web.de>
2018-06-11 04:27:44 +0200

    Compacted Method for creation of the map.

--- Schulprojekt/src/createMap.java
+++ Schulprojekt/src/createMap.java
@@ -21,13 +21,13 @@ public class createMap {
 	private static final int radiusBase = 4;
 	public static MapTile[][] createCustom( int x, int y) {
 		MapTile[][] map = new MapTile[x][y];
-		doStuff(map,0);
+		buildMap(map,0);
 		return map;
 
 	}
 	public static MapTile[][] createDefault(MapTile[][] map) {
-		map = new MapTile[200][200];
-		doStuff(map,0);
+		map = new MapTile[20][20];
+		buildMap(map,0);
 		return map;
 	}
 	private static void placeEverywhere(MapTile[][] map, int type) { // Remove type in final build.
@@ -37,48 +37,40 @@ public class createMap {
 			}
 		}
 	}
-	private static void doStuff(MapTile[][] map, int type) {
-		placeEverywhere(map,type);
-		placeForests(map);
-		placeXX(map);
+	private static void buildMap(MapTile[][] map, int type) {
+		placeX(map,0);
+		placeX(map,1);
+		//placeX(map,2);
 	}
-	private static void placeForests(MapTile[][] map) {
-		Random random = new Random();
-		// first for is going over the map to scan everything, then, if it's in bounds, give it a random chance to create a forest, place forests sporadically.
-		for(int y=0; y < map.length; y++) {
-			for(int x=0; x < map[0].length; x++) {
-				int radius = radiusBase + random.nextInt(3);
-				if((x-radius) >= 0 && (y-radius) >= 0 && (x-radius) < map.length && (y-radius) < map[0].length && (random.nextInt(100)+1) <= 10 ) { 
-					for(int yRadius = 0; yRadius < radius; yRadius++) {
-						for( int xRadius = 0; xRadius < radius; xRadius++ ) {
-							if((random.nextInt(100)+1) <= 30) {
-								map[xRadius+x-radius][yRadius+y-radius] = new MapTile(1);
-							}
-						}
-					}	
-				}
-			}
-		}
-	}
-	private static void placeXX(MapTile[][] map) {
-		Random random = new Random();
-		// first for is going over the map to scan everything, then, if it's in bounds, give it a random chance to create a forest, place forests sporadically.
-		for(int y=0; y < map.length; y++) {
-			for(int x=0; x < map[0].length; x++) {
-				int radius = radiusBase + random.nextInt(3);
-				if((x-radius) >= 0 && (y-radius) >= 0 && (x-radius) < map.length && (y-radius) < map[0].length && (random.nextInt(100)+1) <= 10 ) { 
-					for(int yRadius = 0; yRadius < radius; yRadius++) {
-						for( int xRadius = 0; xRadius < radius; xRadius++ ) {
-							if((random.nextInt(100)+1) <= 30) {
-								map[xRadius+x-radius][yRadius+y-radius] = new MapTile(2);
-							}
-						}
-					}	
-				}
-			}
+	private static void placeX(MapTile[][] map, int type) {
+		switch(type) {
+		case 0: placeEverywhere(map,type);
+				break;
+		case 1: placeRadialType(map, type, 10, 30);
+				break;
+		case 2: placeRadialType(map, type, 20, 5);
+				break;
+		default:placeRadialType(map, 0, 0, 0);
 		}
 	}		
-	
+	private static void placeRadialType(MapTile[][] map, int type, int spawnChance, int tileChance) {
+		Random random = new Random();
+		// first for is going over the map to scan everything, then, if it's in bounds, give it a random chance to create a forest, place forests sporadically.
+		for(int y=0; y < map.length; y++) {
+			for(int x=0; x < map[0].length; x++) {
+				int radius = radiusBase + random.nextInt(3);
+				if((x-radius) >= 0 && (y-radius) >= 0 && (x-radius) < map.length && (y-radius) < map[0].length && (random.nextInt(100)+1) <= spawnChance ) { 
+					for(int yRadius = 0; yRadius <= radius; yRadius++) {
+						for( int xRadius = 0; xRadius <= radius; xRadius++ ) {
+							if((random.nextInt(100)+1) <= tileChance) {
+								map[xRadius+x-radius][yRadius+y-radius] = new MapTile(type);
+							}
+						}
+					}	
+				}
+			}
+		}
+	}
 	
 	
 }
