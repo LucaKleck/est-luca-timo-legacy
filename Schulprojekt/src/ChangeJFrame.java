4e3db0b79a5515a191c6e0ae462b54d76ba1cd50
schueler <schueler@a021pc32.ESTML>
2018-06-18 09:08:37 +0200

    Pi

--- Schulprojekt/src/ChangeJFrame.java
+++ Schulprojekt/src/ChangeJFrame.java
@@ -4,7 +4,7 @@ public class ChangeJFrame {
 	public static void change(JFrame f) {
 		f.setSize(300,400);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.add(new DrawGui(f));
+		f.add(new DrawGui());
 		f.setVisible(true);
 	}
 	
--- Schulprojekt/src/DrawGui.java
+++ Schulprojekt/src/DrawGui.java
@@ -11,21 +11,18 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.LinearGradientPaint;
 import java.awt.geom.Rectangle2D;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class DrawGui extends JPanel {
-	private JFrame f;
-	public DrawGui( JFrame f ) {
-		this.f = f;
-	}
 	private static final long serialVersionUID = 1L;
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		System.out.println("f Width: " + f.getWidth() + "  f Height: " + f.getHeight());
-		Rectangle2D r = new Rectangle2D.Double(0, 0, f.getWidth(), f.getHeight());
-		Paint(g2, r);
-		
+		System.out.println("Width: " + this.getWidth() + "  Height: " + this.getHeight());
+		//Rectangle2D r = new Rectangle2D.Double(0, 0, this.getWidth(), this.getHeight());
+		//Paint(g2, r);
+		for(int x=0; x<=this.getWidth();x++) {
+			int y = Calc.Sinus.getYofX(this.getHeight(),this.getWidth(),x));
+			g.drawLine(x, y, x, y;
+		}
 		
 	}
 	void Paint(Graphics2D g, Rectangle2D r) {
